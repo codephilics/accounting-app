7fc5946b8de8a5d1e5c4e2f98da5797c73a870aa
sakibmukter95 <sakibmukter77@gmail.com>
2020-09-23 16:15:01 +0600

    modal to table data entry done

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -96,4 +96,319 @@ function isAuthenticate() {
 }
 
 
- 
+$("#debit-file-upload").change(function () {
+  var filepath = this.value;
+  var m = filepath.match(/([^\/\\]+)$/);
+  var filename = m[1];
+  $("#debit-filename").html(filename);
+});
+
+$("#credit-file-upload").change(function () {
+  var filepath = this.value;
+  var m = filepath.match(/([^\/\\]+)$/);
+  var filename = m[1];
+  $("#credit-filename").html(filename);
+});
+
+// debit table insertation
+
+$("#debit-submit").on("click", function (event) {
+  event.preventDefault();
+  var debitCompany = $("#dCompany").val();
+  var debitCO_CO = $("#dCO_CO").val();
+  var debitSite = $("#dSite").val();
+  var debitPerson_Car = $("#dPerson-Car-Who-will-Get").val();
+  var debitDepartment = $("#dDepartment").val();
+  var debitCause = $("#dCause").val();
+  var debitCarrier = $("#dCarrier-Driver").val();
+  var debitRefer = $("#dRefer-By").val();
+  var debitAmount = $("#dAmount").val();
+  var debitOtherCost = $("#dOther-Cost").val();
+  var debitTotal = $("#dTotal").val();
+  var debitDena = $("#dDena").val();
+  var debitPaona = $("#dPaona").val();
+  var debitVara = $("#dVara").val();
+  var debitWarning = $("#dWarning").val();
+  var debitNote = $("#dNote").val();
+  var debitEditor = $("#dEdited-By").val();
+  var debitFile = $("#debit-file-upload").val();
+
+  var row = "<tr class ='debit-tr'>";
+
+  var debitSL = $(".debit-tr").length;
+  function updateDebitSL(currentDebitSL) {
+    currentDebitSL = debitSL;
+
+    return currentDebitSL;
+  }
+  row +=
+    "<td>" +
+    updateDebitSL(debitSL) +
+    "</td>" +
+    "<td>" +
+    debitCompany +
+    "</td>" +
+    "<td>" +
+    debitCO_CO +
+    "</td>" +
+    "<td>" +
+    debitSite +
+    "</td>" +
+    "<td>" +
+    debitPerson_Car +
+    "</td>" +
+    "<td>" +
+    debitDepartment +
+    "</td>" +
+    "<td>" +
+    debitCause +
+    "</td>" +
+    "<td>" +
+    debitCarrier +
+    "</td>" +
+    "<td>" +
+    debitRefer +
+    "</td>" +
+    "<td>" +
+    debitAmount +
+    "</td>" +
+    "<td>" +
+    debitOtherCost +
+    "</td>" +
+    "<td class='debit-total-sum'>" +
+    debitTotal +
+    "</td>" +
+    "<td>" +
+    debitDena +
+    "</td>" +
+    "<td>" +
+    debitPaona +
+    "</td>" +
+    "<td>" +
+    debitVara +
+    "</td>" +
+    "<td>" +
+    debitWarning +
+    "</td>" +
+    "<td>" +
+    debitNote +
+    "</td>" +
+    "<td>" +
+    debitEditor +
+    "</td>" +
+    "<td>" +
+    debitFile +
+    "</td>" +
+    "<td>" +
+    "<i class='fas fa-pen edit-item-debit' data-toggle='modal' data-target='#debit-edit-modal' style='cursor: pointer;'></i>" +
+    "<i class='fas fa-trash-alt remove-item-debit' style='padding-left: 8px; cursor: pointer;'></i>" +
+    "</td>";
+  row += "</tr>";
+
+  console.debug(row);
+
+  $(".debit-table .debit-tbody").append(row);
+
+  var total = 0;
+  var ISAresult = 0;
+  var ISAvalue = parseFloat($(".ISA-value-today").text());
+
+  var debitTableSum = $(".debit-total-sum");
+  for (var i = 0; i < debitSL; i++) {
+    total = total + parseFloat(debitTableSum[i].innerHTML);
+    ISAresult = ISAvalue - parseFloat(debitTableSum[i].innerHTML);
+  }
+  $("#debit-sum").html(total);
+
+  console.log(ISAvalue);
+  $(".ISA-value-today").html(ISAresult);
+  console.log($(".debit-amount-sum"));
+
+  console.log($(".debit-tr"));
+
+  $(".debit-tr").on("click", ".remove-item-debit", function () {
+    console.log("hello");
+    $(this).closest("tr").remove();
+    debitSL--;
+    console.log(debitSL);
+    updateDebitSL(debitSL);
+  });
+
+
+});
+
+
+
+// credit table insertation
+
+$("#credit-submit").on("click", function (event) {
+  event.preventDefault();
+  var creditCompany = $("#crCompany").val();
+  var creditCO_CO = $("#crCO_CO").val();
+  var creditSite = $("#crSite").val();
+  var creditPerson_Car = $("#crPerson-Car-Who-will-Get").val();
+  var creditDepartment = $("#crDepartment").val();
+  var creditCause = $("#crCause").val();
+  var creditCarrier = $("#crCarrier-Driver").val();
+  var creditRefer = $("#crRefer-By").val();
+  var creditAmount = $("#crAmount").val();
+  var creditOtherCost = $("#crOther-Cost").val();
+  var creditTotal = $("#crTotal").val();
+  var creditDue = $("#crDue").val();
+  var creditInvest = $("#crInvest").val();
+  var creditNote = $("#crNote").val();
+  var creditEditor = $("#crEdited-By").val();
+  var creditFile = $("#credit-file-upload").val();
+
+  var row = "<tr class ='credit-tr'>";
+  var creditSL = $(".credit-tr").length;
+  row +=
+    "<td>" +
+    creditSL +
+    "</td>" +
+    "<td>" +
+    creditCompany +
+    "</td>" +
+    "<td>" +
+    creditCO_CO +
+    "</td>" +
+    "<td>" +
+    creditSite +
+    "</td>" +
+    "<td>" +
+    creditPerson_Car +
+    "</td>" +
+    "<td>" +
+    creditDepartment +
+    "</td>" +
+    "<td>" +
+    creditCause +
+    "</td>" +
+    "<td>" +
+    creditCarrier +
+    "</td>" +
+    "<td>" +
+    creditRefer +
+    "</td>" +
+    "<td>" +
+    creditAmount +
+    "</td>" +
+    "<td>" +
+    creditOtherCost +
+    "</td>" +
+    "<td class='credit-total-sum'>" +
+    creditTotal +
+    "</td>" +
+    "<td>" +
+    creditDue +
+    "</td>" +
+    "<td>" +
+    creditInvest +
+    "</td>" +
+    "<td>" +
+    creditNote +
+    "</td>" +
+    "<td>" +
+    creditEditor +
+    "</td>" +
+    "<td>" +
+    creditFile +
+    "</td>" +
+    "<td>" +
+    "<i class='fas fa-pen' style='cursor: pointer;'></i>" +
+    "<i class='fas fa-trash-alt remove-item-credit' style='padding-left: 8px; cursor: pointer;'></i>" +
+    "</td>";
+  row += "</tr>";
+
+  console.debug(row);
+
+  $(".credit-table .credit-tbody").append(row);
+
+  var creditTotal = 0;
+  var ISAresult = 0;
+  var ISAvalue = parseFloat($(".ISA-value-today").text());
+
+  var creditTableSum = $(".credit-total-sum");
+  for (var i = 0; i < creditSL; i++) {
+    creditTotal = creditTotal + parseFloat(creditTableSum[i].innerHTML);
+    ISAresult = ISAvalue + parseFloat(creditTableSum[i].innerHTML);
+  }
+  $("#credit-sum").html(creditTotal);
+
+  console.log(ISAvalue);
+  $(".ISA-value-today").html(ISAresult);
+
+  console.log($(".debit-amount-sum"));
+});
+
+$("#create-account-button").on("click", function (event) {
+  event.preventDefault();
+  var accountFullname = $("#acFull-Name").val();
+  var accountID = $("#acID").val();
+  var accountNID = $("#acNID").val();
+  var accountBloodGroup = $("#acBlood-Group").val();
+  var accountFatersName = $("#acFathers-Name").val();
+  var accountMothersName = $("#acMothers-Name").val();
+  var accountPermanentAddress = $("#acPermanent-Address").val();
+  var accountPresentAddress = $("#acPresent-Address").val();
+  var accountOpeningDate = $("#acOpening-Date").val();
+  var accountClosingDate = $("#acClosing-Date").val();
+  var accountNote = $("#acNote").val();
+  var accountPicture = $("#imagePreview").val();
+  var accountCreator = $("#acCreated-By").val();
+
+  var row = "<tr class ='add-account-tr'>";
+  var accountSL = $(".add-account-tr").length;
+  row +=
+    "<td>" +
+    accountSL +
+    "</td>" +
+    "<td>" +
+    accountFullname +
+    "</td>" +
+    "<td>" +
+    accountID +
+    "</td>" +
+    "<td>" +
+    accountNID +
+    "</td>" +
+    "<td>" +
+    accountBloodGroup +
+    "</td>" +
+    "<td>" +
+    accountFatersName +
+    "</td>" +
+    "<td>" +
+    accountMothersName +
+    "</td>" +
+    "<td>" +
+    accountPermanentAddress +
+    "</td>" +
+    "<td>" +
+    accountPresentAddress +
+    "</td>" +
+    "<td>" +
+    accountOpeningDate +
+    "</td>" +
+    "<td>" +
+    accountClosingDate +
+    "</td>" +
+    "<td>" +
+    accountNote +
+    "</td>" +
+    "<td>" +
+    "<img class='profileCellImage' width='100%' height='50%' src='http://dummyimage.com/68x68/000/fff' />" +
+    "</td>" +
+    "<td>" +
+    accountCreator +
+    "</td>" +
+    "<td>" +
+    "<i class='fas fa-pen' style='cursor: pointer;'></i>" +
+    "<i class='fas fa-trash-alt remove-item-account' style='padding-left: 8px; cursor: pointer;'></i>" +
+    "</td>";
+  row += "</tr>";
+
+  console.debug(row);
+
+  $(".add-account-table .add-account-tbody").append(row);
+});
