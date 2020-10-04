2019883e3f2bff8c0c3eb8e896f6fa9248f6b839
sakibmukter95 <sakibmukter77@gmail.com>
2020-10-04 21:06:12 +0600

    search calendar implemented

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1063,3 +1063,111 @@ function creditEditButton(
   $("#c-edit-Note").val(note);
   $("#c-edit-Edited-By").val(editedBy);
 }
+
+// search calendar
+
+var bindDateRangeValidation = function (f, s, e) {
+  if (!(f instanceof jQuery)) {
+    console.log("Not passing a jQuery object");
+  }
+
+  var jqForm = f,
+    startDateId = s,
+    endDateId = e;
+
+  var checkDateRange = function (startDate, endDate) {
+    var isValid =
+      startDate != "" && endDate != "" ? startDate <= endDate : true;
+    return isValid;
+  };
+
+  var bindValidator = function () {
+    var bstpValidate = jqForm.data("bootstrapValidator");
+    var validateFields = {
+      startDate: {
+        validators: {
+          notEmpty: { message: "This field is required." },
+          callback: {
+            message: "Start Date must less than or equal to End Date.",
+            callback: function (startDate, validator, $field) {
+              return checkDateRange(startDate, $("#" + endDateId).val());
+            },
+          },
+        },
+      },
+      endDate: {
+        validators: {
+          notEmpty: { message: "This field is required." },
+          callback: {
+            message: "End Date must greater than or equal to Start Date.",
+            callback: function (endDate, validator, $field) {
+              return checkDateRange($("#" + startDateId).val(), endDate);
+            },
+          },
+        },
+      },
+      customize: {
+        validators: {
+          customize: { message: "customize." },
+        },
+      },
+    };
+    if (!bstpValidate) {
+      jqForm.bootstrapValidator({
+        excluded: [":disabled"],
+      });
+    }
+
+    jqForm.bootstrapValidator(
+      "addField",
+      startDateId,
+      validateFields.startDate
+    );
+    jqForm.bootstrapValidator("addField", endDateId, validateFields.endDate);
+  };
+
+  var hookValidatorEvt = function () {
+    var dateBlur = function (e, bundleDateId, action) {
+      jqForm.bootstrapValidator("revalidateField", e.target.id);
+    };
+
+    $("#" + startDateId).on("dp.change dp.update blur", function (e) {
+      $("#" + endDateId)
+        .data("DateTimePicker")
+        .setMinDate(e.date);
+      dateBlur(e, endDateId);
+    });
+
+    $("#" + endDateId).on("dp.change dp.update blur", function (e) {
+      $("#" + startDateId)
+        .data("DateTimePicker")
+        .setMaxDate(e.date);
+      dateBlur(e, startDateId);
+    });
+  };
+
+  bindValidator();
+  hookValidatorEvt();
+};
+
+$(function () {
+  var sd = new Date(),
+    ed = new Date();
+
+  $("#startDate").datetimepicker({
+    pickTime: false,
+    format: "YYYY/MM/DD",
+    defaultDate: sd,
+    maxDate: ed,
+  });
+
+  $("#endDate").datetimepicker({
+    pickTime: false,
+    format: "YYYY/MM/DD",
+    defaultDate: ed,
+    minDate: sd,
+  });
+
+  //passing 1.jquery form object, 2.start date dom Id, 3.end date dom Id
+  bindDateRangeValidation($("#form"), "startDate", "endDate");
+});
